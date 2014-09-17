88b341a0eeec100da0b513a8401619495e54db57
iliya tryapitsin <iliya.tryapitsin@gmail.com>
2014-09-18 00:27:55 +0400

    ui changes add logo

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,24 +8,29 @@ namespace RealtyStore
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/js")
+                .Include("~/scripts/jquery.inputmask.js",
+                "~/scripts/jquery.inputmask-multi.js",
+                "~/scripts/jquery.inputmask.extensions.js",
+                "~/scripts/jquery.inputmask.numeric.extensions.js",
+                "~/scripts/from.js",
+                "~/scripts/loadMap.js",
+                "~/scripts/darkMapPallete.js",
+                "~/scripts/lightMapPallete.js",
+                "~/scripts/angular-file-upload.js",
+                "~/scripts/app/app.js",
+                "~/scripts/app/directives/inputmask.js",
+                "~/scripts/app/directives/with.js",
+                "~/scripts/app/directives/ngDivOpenFile.js",
+                "~/scripts/app/directives/ngthumbs.js",
+                "~/scripts/app/directives/gmap.js",
+                "~/scripts/app/controllers/baseController.js",
+                "~/scripts/app/controllers/indexController.js",
+                "~/scripts/app/controllers/newAdvertController.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/Content/site.css",
+                      "~/Content/common.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
